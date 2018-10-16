e182fe85447e2ba67b4d7c9fd1c338d6bcc87517
SarahSlean <yoda4@hotmail.fr>
2018-10-16 21:57:28 +0200

    update BusinessList.js for step 21

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -9,8 +9,18 @@ class BusinessList extends React.Component {
   		<div className="BusinessList">
         {this.props.businesses.map( business =>
          <Business business={business}
-          />
-          )}
+                imgSrc={business.imageSrc}
+                name={business.name}
+                address={business.address}
+                city={business.city}
+                state={business.state}
+                zipCode={business.zipCode}
+                category={business.category}
+                rating={business.rating}
+                reviewCount={business.reviewCount}
+                key={business.id}
+         /> 
+        )}
       </div>
       );
   }
